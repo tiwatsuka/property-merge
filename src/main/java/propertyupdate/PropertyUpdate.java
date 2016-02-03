9a2c5d50b107223ad07f9e2e76ab7ad716142496
Takuya Iwatsuka <takuya.iwatsuka@gmail.com>
2016-02-03 16:24:56 +0900

    update using json

--- src/main/java/propertyupdate/PropertyUpdate.java
+++ src/main/java/propertyupdate/PropertyUpdate.java
@@ -1,33 +1,57 @@
 package propertyupdate;
 
-import java.util.Properties;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Properties;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.TreeSet;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PropertyUpdate {
 
     public static void main(String[] args){
 
-        Properties source = new Properties();
-        Properties target = new Properties();
-        Properties dest = new Properties();
-        try(InputStream source_is = new FileInputStream(args[0]);
-            InputStream target_is = new FileInputStream(args[1])){
+        try(InputStream sourceStream = new FileInputStream(args[0]);
+            InputStream targetStream = new FileInputStream(args[1]);
+            InputStream updateDataStream = new FileInputStream(args[0]+".json");){
+
+            Properties source = new Properties();
+            Properties target = new Properties(){
+                    @Override
+                    public synchronized Enumeration<Object> keys(){
+                        return Collections.enumeration(new TreeSet<Object>(super.keySet()));
+                    }
+                };
+            source.load(sourceStream);
+            target.load(targetStream);
             
-            source.load(source_is);
-            target.load(target_is);
-            
-            for (String key : source.stringPropertyNames()){
-                if(target.getProperty(key) == null){
+            ObjectMapper mapper = new ObjectMapper();
+            UpdateData data = mapper.readValue(updateDataStream, UpdateData.class);
+
+            Properties dest = (Properties) target.clone();
+            for (String key : data.getCreateList()){
+                if(target.getProperty(key) == null && source.getProperty(key) != null){
                     dest.setProperty(key, source.getProperty(key));
-                }else{
+                }
+            }
+
+            for (String key : data.getUpdateList()){
+                if(target.getProperty(key) != null && source.getProperty(key) != null){
                     dest.setProperty(key, source.getProperty(key));
+                }
+            }
+
+            for (String key : data.getDeleteList()){
+                if(dest.getProperty(key) != null){
+                    dest.remove(key);
                 }
             }
 
             dest.store(System.out, null);
         } catch (Exception ex){
-            System.out.println(ex);
+            System.err.println(ex);
         }
     }
 }
---
+++ src/main/java/propertyupdate/UpdateData.java
@@ -0,0 +1,36 @@
+package propertyupdate;
+
+import java.util.List;
+
+public class UpdateData{
+
+    private List<String> createList;
+
+    private List<String> updateList;
+
+    private List<String> deleteList;
+
+    public void setCreateList(List<String> createList){
+        this.createList = createList;
+    }
+
+    public void setUpdateList(List<String> updateList){
+        this.updateList = updateList;
+    }
+
+    public void setDeleteList(List<String> deleteList){
+        this.deleteList = deleteList;
+    }
+
+    public List<String> getCreateList(){
+        return createList;
+    }
+
+    public List<String> getUpdateList(){
+        return updateList;
+    }
+
+    public List<String> getDeleteList(){
+        return deleteList;
+    }
+}
